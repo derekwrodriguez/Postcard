a970b5cd6bc8b59d6fd19f4a49728a78f0f6b834
derekwrodriguez <dwr789@hotmail.com>
2014-10-07 23:11:47 -0400

    Post Card App for Prog 2
    Tutorial thingy! UI Galore!

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var mailButton: UIButton!
+    @IBOutlet var enterMessageTextField: UITextField!
+    @IBOutlet var EnterNameTextField: UITextField!
+    @IBOutlet var messageLabel: UILabel!
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden  = false;
+        messageLabel.text = enterMessageTextField.text;
+        enterMessageTextField.text = "";
+        messageLabel.textColor = UIColor.blueColor()
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal );
+        //Code will evaluate when we press this sucker.
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
