0e3f5984f5a4ebda56a14981f681e5047629da73
derekwrodriguez <dwr789@hotmail.com>
2014-10-08 01:46:17 -0400

    New Label
    New Label with matching functionality and different color

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,13 +14,18 @@ class ViewController: UIViewController {
     @IBOutlet var enterMessageTextField: UITextField!
     @IBOutlet var EnterNameTextField: UITextField!
     @IBOutlet var messageLabel: UILabel!
-
+    @IBOutlet var nameLabel: UILabel!
+    
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         //GITHUB IS FOR PIMPS
         messageLabel.hidden  = false;
+        nameLabel.hidden = false;
         messageLabel.text = enterMessageTextField.text;
+        nameLabel.text = EnterNameTextField.text;
         enterMessageTextField.text = "";
+        EnterNameTextField.text = "";
         messageLabel.textColor = UIColor.blueColor()
+        nameLabel.textColor = UIColor.redColor();
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal );
         //Code will evaluate when we press this sucker.
     }
